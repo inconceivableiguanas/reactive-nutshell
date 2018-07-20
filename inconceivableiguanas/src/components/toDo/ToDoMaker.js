98573a5cb65ebd679f52c2aa3424a43f633313a8
leahgwin <leahgwin@gmail.com>
2018-07-20 15:37:05 -0500

    added more of the form for todolist

--- inconceivableiguanas/src/components/toDo/ToDoMaker.js
+++ inconceivableiguanas/src/components/toDo/ToDoMaker.js
@@ -3,12 +3,54 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
 class TodoList extends Component {
+  //getting ready to intialize local state and bind event handler
+  constructor(props) {
+    //used to access and call functions on the parent
+    super(props);
+    //setting local state here
+    this.state = {
+      items: []
+    };
+    //ensure the 'this' keyword resolves properly
+    this.addItem = this.addItem.bind(this);
+  }
+  //   define our addItem event handler
+  addItem(e) {
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+
+      this.setState(prevState => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+
+      this._inputElement.value = "";
+    }
+
+    console.log(this.state.items);
+
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
+          {/* calls the event handler on submit */}
           <form onSubmit={this.addItem}>
-            <input placeholder="enter task" />
+            <input
+              // storing a reference to our input element
+              ref={a => (this._inputElement = a)}
+              placeholder="enter task"
+            />
+            <input
+              ref={a => (this._inputElement = a)}
+              placeholder="enter date"
+            />
             <button type="submit">add</button>
           </form>
         </div>
