449d9158bf193cbc7bd898a20ef9fccccc6df203
Shuaib Sajid <shusajid@gmail.com>
2018-07-22 15:22:00 -0500

    messing around with bloomer

--- inconceivableiguanas/src/components/nav/NavBar.js
+++ inconceivableiguanas/src/components/nav/NavBar.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-// import "bulma/css/bulma.css";
+import "bulma/css/bulma.css";
+import { Navbar,NavbarItem} from 'bloomer';
 
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
+      <Navbar style={{ border: 'solid 1px blue', margin: '0' }}>
+      <NavbarItem>
         <Link to="/">Home</Link>
         <Link to="/events">Events</Link>
         <Link to="/todo">To Do</Link>
         <Link to="/articles">Articles</Link>
         <Link to="/friends">Friends</Link>
         <Link to="/chat">Chat</Link>
-      </div>
+      </NavbarItem>
+      </Navbar>
     );
   }
 }
