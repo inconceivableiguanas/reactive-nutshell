270f5073c6704ed31b9e9eac526c751cca3b4094
leahgwin <leahgwin@gmail.com>
2018-07-24 10:04:32 -0500

    working on checkbox

--- inconceivableiguanas/src/components/toDo/TodoCheckFn.js
+++ inconceivableiguanas/src/components/toDo/TodoCheckFn.js
@@ -1,15 +1,15 @@
-//leah gwin 2018
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
+// //leah gwin 2018
+// import React, { Component } from "react";
+// import { Route } from "react-router-dom";
 
-export default class check extends Component {
-  // console.log(props, "props")
-  return (
-    //passing in id, getting tasks and setting state
-    (checkTask = task => {
-      ApiManager.patchItem(task).then(getTasks => {
-        ApiManager.getAll().then(tasks => this.setState({ tasks: tasks }));
-      });
-    })
-  );
-};
+// export default class check extends Component {
+//   // console.log(props, "props")
+//   return (
+//     //passing in id, getting tasks and setting state
+//     (checkTask = task => {
+//       ApiManager.patchItem(task).then(getTasks => {
+//         ApiManager.getAll().then(tasks => this.setState({ tasks: tasks }));
+//       });
+//     })
+//   );
+// };
