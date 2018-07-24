0372d12939ab7c6c7cb76a7b087f715f96d57d31
Aaron Miller <millerac333@gmail.com>
2018-07-24 11:48:21 -0500

    rewrote function

--- inconceivableiguanas/src/components/events/EventsForm.js
+++ inconceivableiguanas/src/components/events/EventsForm.js
@@ -21,52 +21,66 @@ export default class EventsForm extends Component {
     stateToChange[event.target.id] = event.target.value;
     this.setState(stateToChange);
   };
+  handleUpdate = e => {
+    e.preventDefault();
+
+    const updatedEvent = {
+      name: this.state.name,
+      placeOf: this.state.placeOf,
+      date: this.state.date
+    };
+    APIManager.patchItem("events", this.props.event.id, updatedEvent).then(
+      () => {
+        this.props.history.push("/events");
+      }
+    );
+  };
 
   // handleSubmit = event => {
   //   alert(this.state.name);
   //   event.preventDefault();
 
-  eventsFunction = () => {
-    if (this.props.location.state.hasOwnProperty("event")) {
-      console.log("edit");
-      let eventsName = this.props.state.name;
-      let eventsLocation = this.props.state.placeOf;
-      let eventsDate = this.props.state.date;
-      let eventsId = this.props.location.state.event.id;
-      let body = {
-        name: eventsName,
-        placeOf: eventsLocation,
-        date: eventsDate
-      };
-      console.log(body);
-      APIManager.patchItem("events", eventsId, body).then(() => {
-        //redirect to events page;
-        // this.handleChange();
-        this.props.history.push("events");
-      });
-    } else {
-      console.log("add");
-      let eventsName = document.getElementById("name").value();
-      let eventsLocation = document.getElementById("placeOf").value();
-      let eventsDate = document.getElementById("date").value();
-      let body = {
-        name: eventsName,
-        placeOf: eventsLocation,
-        date: eventsDate
-      };
-      console.log(eventsDate);
-      APIManager.addData("events", body).then(() => {
-        this.props.history.push("/events");
-      });
-    }
-  };
+  // eventsFunction = () => {
+  //   if (this.props.location.state.hasOwnProperty("event")) {
+  //     console.log("edit");
+  //     let eventsName = this.props.state.name;
+  //     let eventsLocation = this.props.state.placeOf;
+  //     let eventsDate = this.props.state.date;
+  //     let eventsId = this.props.location.state.event.id;
+  //     let body = {
+  //       name: eventsName,
+  //       placeOf: eventsLocation,
+  //       date: eventsDate
+  //     };
+  //     console.log(body);
+  //     APIManager.patchItem("events", eventsId, body).then(() => {
+  //       //redirect to events page;
+  //       // this.handleChange();
+  //       this.props.history.push("events");
+  //     });
+  //   } else {
+  //     console.log("add");
+  //     let eventsName = document.getElementById("name").value();
+  //     let eventsLocation = document.getElementById("placeOf").value();
+  //     let eventsDate = document.getElementById("date").value();
+  //     let body = {
+  //       name: eventsName,
+  //       placeOf: eventsLocation,
+  //       date: eventsDate
+  //     };
+  //     console.log(eventsDate);
+  //     APIManager.addData("events", body).then(() => {
+  //       this.props.history.push("/events");
+  //     });
+  //   }
+  // };
 
   editEventForm = () => {
     return (
       // if (this.state.clicked === "") {
       //   this.setState({
       //     clicked: (
-      <form onSubmit={this.eventsFunction}>
+      <form onSubmit={this.handleUpdate}>
         <label>Event Title</label>
         <input
           id="name"
